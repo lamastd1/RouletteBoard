a67961c8678b1376040de2464f6b57436cdf50f9
Dom Lamastra <dlamastra44@gmail.com>
2024-10-16 09:30:45 -0400

    made bet csv file with bet info

--- Sources/main.swift
+++ Sources/main.swift
@@ -254,5 +254,11 @@ let boardCsv: String = csvWriter.createCSV(from: pieces, using: boardHeaders) { 
 }
 csvWriter.writeCSV(to: "board.csv", content: boardCsv)
 
+let betHeaders: [String] = ["name", "odds", "payout"]
+let betCsv: String = csvWriter.createCSV(from: betPlacements, using: betHeaders) { betPlacement in 
+  return [betPlacement.name, "\(betPlacement.odds)", "\(betPlacement.payout)"]
+}
+csvWriter.writeCSV(to: "bet.csv", content: betCsv)
+
 let fibonacciHeaders: [String] = ["rounds", "profit", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "0", "00"]
 
